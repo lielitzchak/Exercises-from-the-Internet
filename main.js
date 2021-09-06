5c27934771c479cd66d1964df15783184416b78c
Liel <liel.izcha321@gmail.com>
2021-09-06 12:50:34 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function getMiddle(s) {
+  for (var i = 0; i < s.length; i++) {
+    if (s % 2 == 1) {
+      console.log(i);
+    } else if (s % 2 == 0) {
+      console.log(i);
+    }
+  }
+}
+getMiddle("liel");
